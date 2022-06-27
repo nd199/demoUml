f486057b191ff76c264636ceda402594b0e5a5c7
Naren D <narendeva001@gmail.com>
2022-06-27 12:22:38 +0530

    Initial commit

---
+++ src/main/java/com/example/demouml/DemoUmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demouml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoUmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demouml/DemoUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demouml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
